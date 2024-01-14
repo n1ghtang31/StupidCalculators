88937e34d53d3dc71ad44ebb8121ec73fd87b0cf
n1ghtang31 <crossiter0@gmail.com>
2024-01-14 15:00:19 -0600

    Finished getting variables from text boxes and added Submit button. Next will be adding print out on page of results

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+//////////////Reverse Sales Tax Calculator////////////////////////
+const getrstPriceWithTax = document.querySelector("#rstPriceWithTax");
+const getrstTaxRate = document.querySelector("#rstTaxRate")
+
+let rstPriceWithTax = getrstPriceWithTax.value;
+let rstTaxRate = getrstTaxRate.value;
+
+getrstPriceWithTax.addEventListener("change", () => {
+    rstPriceWithTax = getrstPriceWithTax.value;
+});
+
+getrstTaxRate.addEventListener("change", () => {
+    rstTaxRate = getrstTaxRate.value;
+});
+
 function calculateReverseSalesTax(totalPrice, salesTax) {
+    console.log(`Entering ${totalPrice} as the total and ${salesTax} as the tax rate`)
     let salesTaxPercentage = parseFloat(salesTax, 10) / 100;
     
     let listPrice = totalPrice / (1 + salesTaxPercentage);
