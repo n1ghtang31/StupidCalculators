197fdcffbe6e359bc0c290ee99564e720d6f9418
n1ghtang31 <crossiter0@gmail.com>
2024-01-14 20:50:42 -0600

    UGH

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const getrstTaxRate = document.querySelector("#rstTaxRate")
 
 let rstPriceWithTax = getrstPriceWithTax.value;
 let rstTaxRate = getrstTaxRate.value;
+let listPrice = '0';
 
 getrstPriceWithTax.addEventListener("change", () => {
     rstPriceWithTax = getrstPriceWithTax.value;
@@ -17,7 +18,8 @@ function calculateReverseSalesTax(totalPrice, salesTax) {
     console.log(`Entering ${totalPrice} as the total and ${salesTax} as the tax rate`)
     let salesTaxPercentage = parseFloat(salesTax, 10) / 100;
     
-    let listPrice = totalPrice / (1 + salesTaxPercentage);
+    listPrice = totalPrice / (1 + salesTaxPercentage);
     console.log(listPrice);
     return listPrice;
-}
+};
+
