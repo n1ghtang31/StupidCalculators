a1c3589be2887b4ff5f0b57f417ea79d103b8d6e
n1ghtang31 <crossiter0@gmail.com>
2024-01-15 10:27:24 -0600

    Finish result fields

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 //////////////Reverse Sales Tax Calculator////////////////////////
 const getrstPriceWithTax = document.querySelector("#rstPriceWithTax");
-const getrstTaxRate = document.querySelector("#rstTaxRate")
+const getrstTaxRate = document.querySelector("#rstTaxRate");
+const displaytotalBeforeTax = document.querySelector("#totalBeforeTax");
+const displaytaxedAmount = document.querySelector("#taxedAmount");
+const displayrstTotal = document.querySelector("#rstTotal");
+
 
 let rstPriceWithTax = getrstPriceWithTax.value;
 let rstTaxRate = getrstTaxRate.value;
@@ -14,12 +18,19 @@ getrstTaxRate.addEventListener("change", () => {
     rstTaxRate = getrstTaxRate.value;
 });
 
+function updateDisplay() {
+    displaytotalBeforeTax.innerText = `Price before Tax: $${rstPriceWithTax}`;
+    displaytaxedAmount.innerText = `Sales Tax: ${rstTaxRate}%`;
+    displayrstTotal.innerText =`Total Amount: $${listPrice}`;
+}
+
 function calculateReverseSalesTax(totalPrice, salesTax) {
     console.log(`Entering ${totalPrice} as the total and ${salesTax} as the tax rate`)
     let salesTaxPercentage = parseFloat(salesTax, 10) / 100;
-    
     listPrice = totalPrice / (1 + salesTaxPercentage);
+    listPrice = listPrice.toFixed(2);
     console.log(listPrice);
+    updateDisplay();
     return listPrice;
 };
 
